e12e051d06316a60b8cb790693875302b983024e
Chaitanyamanepalli <chaitanyamanepalli130@gmail.com>
2025-11-26 07:54:10 +0530

    Implement windows in dock

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,9 +3,11 @@ import { Tooltip } from 'react-tooltip'
 import { dockApps } from "#constants/index.js"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import useWindowStore from "#store/window.js"
 
 
 const Dock = () => {
+    const {openWindow,closeWindow,windows} = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -59,7 +61,15 @@ const Dock = () => {
     }, []);
 
     const toggleApp = (app) => {
-          
+          if(!app.canOpen) return;
+
+          const window = windows[app.id];
+          if(window.isOpen){
+            closeWindow(app.id);
+          }else{
+            openWindow(app.id);
+          }
+          console.log(windows);
     };
 
     return (
